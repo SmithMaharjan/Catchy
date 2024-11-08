ca50950bf683beb6fdd9cac2b856377e89f6475f
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-11-08 19:11:26 +0545

    album context having albums state

---
+++ src/context/AlbumState.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const AlbumContext = createContext()
+const AlbumContextProvider = ({ children }) => {
+    const [albums, setAlbums] = useState([{
+        name: "",
+        user: "",
+        musics: []
+    }])
+    // const [musics, setMusics] = useState([])
+    const value = {
+        albums, setAlbums
+    }
+
+
+    return (
+        <AlbumContext.Provider value={value}>
+            {children}
+        </AlbumContext.Provider>
+    )
+}
+export default AlbumContextProvider
